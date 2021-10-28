4b4867de01dddff68186a0648bc3692265a4a150
iggbom <jonas@curity.io>
2021-10-28 09:19:42 -0700

    Updated tests

--- src/test/com/example/curity/SendGridEmailSender/SendgridEmailSenderTest.groovy
+++ src/test/com/example/curity/SendGridEmailSender/SendgridEmailSenderTest.groovy
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 2021 Curity AB
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.example.curity.SendGridEmailSender
 
 import se.curity.identityserver.sdk.data.email.RenderableEmail
@@ -19,11 +34,8 @@ class SendgridEmailSenderTest extends Specification {
                 Mock(ExceptionFactory)
         )
 
-        when:
-        def isValid = SendGridEmailSender.isValidEmailAddress(config._defaultSender)
-
-        then:
-        isValid == true
+        expect:
+        SendGridEmailSender.isValidEmailAddress(config._defaultSender) == true
     }
 
     def "send email"() {
@@ -38,11 +50,8 @@ class SendgridEmailSenderTest extends Specification {
         def email = getEmail()
         def recipient = recipient
 
-        when:
+        expect:
         sender.sendEmail(email, recipient)
-
-        then:
-        "Check for email"
     }
 
     def "incorrect api key"() {
